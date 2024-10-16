865a7630b3c56c96a0a86615c910ae4422a31301
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2024-10-16 09:36:19 -0300

    feat: 🎸 country search

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,13 @@
-import Link from 'next/link';
-
 import { listCountries } from '@/actions/country/list';
-import { CountryCard, PageContent } from '@/components';
+import { PageContent } from '@/components';
+import { CountryList } from '@/components/CountryList';
 
 export default async function Home() {
   const countries = await listCountries(2000);
 
   return (
-    <PageContent className="grid grid-cols-1 gap-10 px-4 md:grid-cols-2 lg:grid-cols-3 lg:px-0 xl:grid-cols-4">
-      {countries.map((country) => (
-        <Link href={`/country/${country.code}`} key={country.name}>
-          <CountryCard name={country.name} flag={country.flag} />
-        </Link>
-      ))}
+    <PageContent>
+      <CountryList data={countries} />
     </PageContent>
   );
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export { Navbar } from './Navbar';
+export { BackButton } from './BackButton';
 export { PageContent } from './PageContent';
 export { CountryCard } from './CountryCard';
 export { Skeleton } from './Skeleton';
